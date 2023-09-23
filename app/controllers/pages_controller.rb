379b704a64be8ddcd872a42aaa77f2d9b184a32c
krillan <pushkinidze49@gmail.com>
2023-09-23 11:23:48 +0300

    about us stiles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def about
     @heading = 'Страница о нас'
     @text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, alias nesciunt, labore earum hic blanditiis quos aspernatur quas unde voluptatibus eligendi possimus a quae, natus nostrum perferendis sapiente totam odio!'
+    @info = 'https://itproger.com/course/ruby-on-rails'
   end
 end
